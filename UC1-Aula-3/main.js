6be523bfc54cfb9212b405d259be84e03dedee8f
Matheus Medtler Alves <ma284163@gmail.com>
2024-12-06 21:55:11 -0300

    Add files via upload

---
+++ UC1-Aula-3/main.js
@@ -0,0 +1,62 @@
+let a = true
+let b = false
+
+let resultado = a && b
+
+console.log(a,'&&',b, '=',resultado)
+
+let resultado9 = ! true 
+console.log(resultado9)
+
+
+let numero1 = 3
+let numero3 = 2
+
+let resultado3 = numero1 == numero3
+
+console.log(numero1,'==',numero3)
+
+
+let number1 = 2
+let number2 = 3
+
+let resultado4 = number1 !== number2
+
+console.log(number1,'!==',number2)
+
+
+let o = 20
+let w = 10 
+
+let resultado5 = o > w 
+console.log(o,'>',w)
+
+
+let p = -1
+let y = 10
+
+let resultado6 = p < y 
+console.log(p,'<',y)
+
+let e = 9
+let n = 9
+
+let resultado7 = e >= n 
+console.log(e,'>=',n)
+
+let g = -2
+let k = 10
+ 
+let resultado8 = g <= k 
+console.log(g,'<=',k)
+
+
+let idade = 20
+let status = idade >= 18 ? 'Adulto' : 'Menor de idade'
+console.log(status)
+
+
+var palavra1 = 'alfa'
+var palavra2 = 'beto'
+var resultado10 = palavra1+palavra2
+console.log(resultado10)
